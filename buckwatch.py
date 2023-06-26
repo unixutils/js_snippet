dd0f5846aaf03edbc7ad8a9fc1739a63f41e4fa9
Vijayendar Gururaja <39288120+unixutils@users.noreply.github.com>
2023-06-26 15:32:38 +0530

    Update buckwatch.py

--- buckwatch.py
+++ buckwatch.py
@@ -1,6 +1,7 @@
 import os
 from google.cloud import storage
 from google.auth import compute_engine
+import filecmp
 
 # Define the GCS bucket and file information
 BUCKET_NAME = "YOUR_BUCKET_NAME"
@@ -15,7 +16,23 @@ blob = bucket.blob(FILE_NAME)
 
 # Check if the file exists in the bucket
 if blob.exists():
-    # Download the file
-    blob.download_to_filename(FILE_NAME)
-    # Delete the file from the bucket
-    blob.delete()
+    # Check if the existing file exists
+    if os.path.exists(FILE_NAME):
+        # Download the file to compare
+        blob.download_to_filename(FILE_NAME + ".new")
+
+        # Compare the contents with the existing file
+        if not filecmp.cmp(FILE_NAME, FILE_NAME + ".new"):
+            # Contents are different, replace the existing file
+            os.rename(FILE_NAME + ".new", FILE_NAME)
+            print("File replaced.")
+        else:
+            # Contents are the same, log a message
+            os.remove(FILE_NAME + ".new")
+            print("Same file exists, not replaced.")
+    else:
+        # Download the file as there is no existing file
+        blob.download_to_filename(FILE_NAME)
+        print("File downloaded.")
+else:
+    print("File not found in the bucket.")
